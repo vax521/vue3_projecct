fd43c71fd7bc67d64d553c187c1eb91b604ad2ac
feimeng <13263397018@163.com>
2020-04-19 15:13:09 +0800

    添加vue-router依赖

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
